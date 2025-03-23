3f6806a3fcb549cc7e7c3605f9830e69a686aafb
Theresa <theresa@Mac.attlocal.net>
2025-03-23 11:37:13 -0700

    working on templating newest tasks carousel card

--- src/app/tasks/add/page.js
+++ src/app/tasks/add/page.js
@@ -38,42 +38,45 @@ export default function AddTaskForm() {
     }
 
     return (
-        <form action={handleFormSubmit}>
-            <input type="hidden" name="user_id" value={userId}/>
-            <input type="hidden" name="task_id" value={userId + Date.now()}/>
-            <input type="text" name="task_name" defaultValue="Task name" />
-            <input type="datetime-local" name="task_due_date" defaultValue="Date" />
-            <label htmlFor="task_repeat">Should this task repeat?</label>
-            <input type="radio" name="task_repeat" value="yes"/><label htmlFor="task_repeat">Yes</label>
-            <input type="radio" name="task_repeat" value="no" defaultChecked/><label htmlFor="task_repeat">No</label>
-            <label htmlFor="task_frequency_cadence">Reminders: How often?</label>
-            <select name="task_frequency_cadence">
-                <option value="daily">Days</option>
-                <option value="weekly">Weekly</option>
-                <option value="monthly">Monthly</option>
-            </select>
-            <label htmlFor="task_frequency_interval">Reminders: How many times per?</label>
-            <input type="number" name="task_frequency_interval"/>
-            <fieldset>
-                <legend>Reminders: What days?</legend>
-                {weekdays.map((day) => (
-                    <div key={day}>
-                        <label htmlFor={`day_${day}`}>{day}</label>
-                        <input type="checkbox"
-                               id={`day_${day}`}
-                               name="task_frequency_daysofweek"
-                               value={day}/>
-                    </div>
-                ))}
-            </fieldset>
-            <textarea name="task_notes" defaultValue="Notes"></textarea>
-            <label htmlFor="task_folder">Folder</label>
-            <select name="task_folder">
-                {folders && folders.map((folder) => (
-                    <option key={Math.random()} value={folder}>{folder}</option>
-                ))}
-            </select>
-            <input type="submit" value="Add Task" />
-        </form>
+        <div>
+            <h2>Add Task:</h2>
+            <form action={handleFormSubmit}>
+                <input type="hidden" name="user_id" value={userId}/>
+                <input type="hidden" name="task_id" value={userId + Date.now()}/>
+                <input type="text" name="task_name" defaultValue="Task name" />
+                <input type="datetime-local" name="task_due_date" defaultValue="Date" />
+                <label htmlFor="task_repeat">Should this task repeat?</label>
+                <input type="radio" name="task_repeat" value="yes"/><label htmlFor="task_repeat">Yes</label>
+                <input type="radio" name="task_repeat" value="no" defaultChecked/><label htmlFor="task_repeat">No</label>
+                <label htmlFor="task_frequency_cadence">Reminders: How often?</label>
+                <select name="task_frequency_cadence">
+                    <option value="daily">Days</option>
+                    <option value="weekly">Weekly</option>
+                    <option value="monthly">Monthly</option>
+                </select>
+                <label htmlFor="task_frequency_interval">Reminders: How many times per?</label>
+                <input type="number" name="task_frequency_interval"/>
+                <fieldset>
+                    <legend>Reminders: What days?</legend>
+                    {weekdays.map((day) => (
+                        <div key={day}>
+                            <label htmlFor={`day_${day}`}>{day}</label>
+                            <input type="checkbox"
+                                   id={`day_${day}`}
+                                   name="task_frequency_daysofweek"
+                                   value={day}/>
+                        </div>
+                    ))}
+                </fieldset>
+                <textarea name="task_notes" defaultValue="Notes"></textarea>
+                <label htmlFor="task_folder">Folder</label>
+                <select name="task_folder">
+                    {folders && folders.map((folder) => (
+                        <option key={Math.random()} value={folder}>{folder}</option>
+                    ))}
+                </select>
+                <input type="submit" value="Add Task" />
+            </form>
+        </div>
     )
 }
